027d8c42aa4e4bd71a78e9cf15b3586cbd111843
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-02-23 10:24:48 -0500

    Don't attempt resize if leadimage data isn't a valid image.

--- agsci/leadimage/content/events.py
+++ agsci/leadimage/content/events.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw
-from StringIO import StringIO 
+from StringIO import StringIO
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 from Products.CMFCore.utils import getToolByName
 
@@ -7,7 +7,7 @@ def rescaleImage(context, event):
 
     max_width = 900.0
     max_height = 1200.0
-    
+
     leadimage = getattr(context, 'leadimage', None)
 
     if leadimage:
@@ -20,20 +20,24 @@ def rescaleImage(context, event):
         }.get(leadimage.contentType, 'UNK')
 
         ratio = min([max_width/w, max_height/h])
-        
+
         if ratio < 1.0:
             new_w = w * ratio
             new_h = h * ratio
 
-            pil_image = Image.open(StringIO(leadimage.data))
-            pil_image.thumbnail([new_w, new_h], Image.ANTIALIAS)
+            try:
+                pil_image = Image.open(StringIO(leadimage.data))
+            except IOError:
+                pass
+            else:
+                pil_image.thumbnail([new_w, new_h], Image.ANTIALIAS)
 
-            img_buffer = StringIO()    
+                img_buffer = StringIO()
 
-            pil_image.save(img_buffer, leadimage_format, quality=100)
+                pil_image.save(img_buffer, leadimage_format, quality=100)
 
-            img_value = img_buffer.getvalue()
-            
-            leadimage._setData(img_value)
+                img_value = img_buffer.getvalue()
 
-        
+                leadimage._setData(img_value)
+
+
